45f2f011a3ad7fa6ecc66a414e6dc4798a873e26
Michael W <walkerrepair255@yahoo.com>
2022-03-13 11:41:01 -0400

    Add project files.

---
+++ NoodlesForRef/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace NoodlesForRef
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give a number between -10 and 10...:");
+            var i = Convert.ToInt32(Console.ReadLine());
+            if (i > -10 && i < 10)
+            {
+                Console.WriteLine("i IS within the permited range.");
+            }else
+            {
+                Console.WriteLine("i NOT within the permited range.");
+            }
+        }
+    }
+}
